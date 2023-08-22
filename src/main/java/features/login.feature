2c7baa1a689890620fc9efcceb4a0b2a85cd62e1
Lajja Patel <lajjapatel8@gmail.com>
2023-08-22 13:26:26 -0400

    from things to do list

---
+++ src/main/java/features/login.feature
@@ -0,0 +1,18 @@
+#Feature file for CucumberTestNG project
+# Concept of Cucumber BDD with TestNG
+
+Feature: Free CRM Login Feature
+
+Scenario Outline: Free CRM Login Test Scenario
+
+Given user is already on Login Page
+When title of login page is Free CRM
+Then user enters "<username>" and "<password>"
+Then user clicks on login button
+Then user is on home page
+Then Close the browser
+
+
+Examples:
+	| username | password |
+	| lajjacrm | Crm@3794 |
---
+++ src/main/java/myRunner/TestRunner.java
@@ -0,0 +1,55 @@
+//Test Runner for CucumberTestNG project
+//To run this code it is preferred to use cmd prompt 
+//navigate to your project in cmd and type >mvn clean install
+//to see reports there are 2 types of reports in target folder refresh project and see
+//1. target > cucumber-pretty > index.html
+//2. target > cucumber-reports > advanced-reports > cucumber-html-reports > overview-features.html
+
+//if u get error =Cannot find class in classpath --> do Project-Clean 
+package myRunner;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+
+@CucumberOptions(
+        features = "C:\\Users\\Lajja Patel\\OneDrive\\Desktop\\MySelenium57\\Cucumber\\CucumberWorkspace\\CucumberTestNG\\src\\main\\java\\features\\login.feature",
+        glue = {"stepDefinitions"},
+        tags = {"~@Ignore"},
+        format = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        },plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+
+public class TestRunner {
+
+	private TestNGCucumberRunner testNGCucumberRunner;
+	 
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+ 
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+ 
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+ 
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+    }
+    
+}
